aab5249cc0312fa420cfb3054cb34db43749c1a9
unknown <saikumarar104@gmail.com>
2021-12-22 21:56:59 +0530

    added product middleware

---
+++ src/Middleware/product.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+require("dotenv").config();
+const {ResponseBody} = require('../utils/response')
+
+exports.product_middleware=function (req, res, next) {
+  const authHeader = req.headers['authorization']
+  if (authHeader == null) return res.sendStatus(401)
+  const token = authHeader.split(' ')[1]
+  jwt.verify(token, process.env.JWT_KEY , (err,user) => {
+    if (err) 
+    {
+      return res.sendStatus(403)
+    } 
+    else
+    {
+    if(req.body.productname && req.body.price && req.body.description)
+    { 
+      req.user = user
+      next()
+    }
+    else
+    {
+      const response = new ResponseBody(false, "please fill all required fields", {});
+      res.send(response);
+    }}
+    
+  })
+}
