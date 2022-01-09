5dcb0139ce403d5dc083d89965b8921022740656
Sai Ar <saikumarar104@gmail.com>
2022-01-09 11:57:12 +0530

    'modified_home_route'

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -73,8 +73,8 @@ async function profile(req,res){
 
 async function home_page(req,res){
     try{
-        products=await product.findAll({limit:4,order:[['createdAt','DESC']]})
-        courses=await course.findAll({limit:4,order:[['createdAt','DESC']]})
+        products=await product.findAll({limit:3,order:[['createdAt','DESC']]})
+        courses=await course.findAll({limit:3,order:[['createdAt','DESC']]})
         const response = new ResponseBody(true, "profile fetched sucessfully",{products,courses});
         res.send(response)
     }
