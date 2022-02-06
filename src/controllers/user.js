e316b7bb6b6dff3e92da8e5f80f3f00f47605ab4
Sai Ar <saikumarar104@gmail.com>
2022-02-06 19:38:57 +0530

    modified

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -67,7 +67,7 @@ async function profile(req,res){
         // const user_address=await address.findAll({attributes:['Address'],where:{userId:User.userid}});
         user_profile={name:User.name,email:User.email,mobile:User.mobile}
         const mycourse = await purchasedcourses.findAll({attributes:['courseid']},{where:{userid:User.userid}});
-        const myproduct=await orders.findAll({where:{userid:User.userid}})
+        const myproduct=await orders.findAll({attributes:['items','status','orderid','address']},{where:{userid:User.userid}})
   
         const response = new ResponseBody(true, "profile fetched sucessfully",{user_profile,mycourse,myproduct});
         res.send(response)
