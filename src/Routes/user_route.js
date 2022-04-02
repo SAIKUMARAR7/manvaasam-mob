7cd622927fc41cc4225618baf2f0366493f10345
Saikumar AR <42142731+SAIKUMARAR7@users.noreply.github.com>
2022-04-02 12:20:33 +0530

    Added Sample routes for checking /nirmal

--- src/Routes/user_route.js
+++ src/Routes/user_route.js
@@ -8,4 +8,9 @@ router.post('/profile',verify.authenticateToken,user.profile);
 router.post('/home_page',user.home_page);
 router.get('/checktoken',verify.authenticateToken,user.checktoken);
 
+//sample to check
+router.get('/checker',(req,res)={
+  res.send('Sample Routes working Fine')
+})
+
 module.exports=router;
